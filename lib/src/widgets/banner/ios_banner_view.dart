063d057cbf67bbd86940bf352e2caedf7006ec32
Alex Sungurov <alexander.sungurov@inappstory.com>
2026-02-26 13:30:30 +0300

    fix: removed unused import

--- lib/src/widgets/banner/ios_banner_view.dart
+++ lib/src/widgets/banner/ios_banner_view.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
-import '../../controllers/banner_place_manager.dart';
 import '../base/banner_platform_view.dart';
 
 class IosBannerView extends BannerPlatformView {
